85897f8e7e062aa553eba7f981fb32dfe3d6b14c
Dr-dyrane <halodyrane@gmail.com>
2023-11-18 09:18:53 +0100

    new file:   src/diagnoses.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,82 +1,116 @@
 // App.jsx
-import React, { useState, useEffect } from 'react';
-import Prescription from './Prescription';
+import React, { useState, useEffect } from "react";
+import Prescription from "./Prescription";
+import diagnosesData from "../diagnoses.json";
 
 function App() {
-  const [weight, setWeight] = useState('');
-  const [age, setAge] = useState('');
-  const [showPrescription, setShowPrescription] = useState(false);
+	const [weight, setWeight] = useState("");
+	const [age, setAge] = useState("");
+	const [selectedDiagnosis, setSelectedDiagnosis] = useState("malariaChild"); // Renamed to avoid conflict
+	const [showPrescription, setShowPrescription] = useState(false);
 
-  const calculateWeightFromAge = (age) => {
-    // Placeholder logic for calculating weight from age
-    return 2 * age + 8;
-  };
+	const calculateWeightFromAge = (age) => {
+		return 2 * age + 8;
+	};
 
-  const handleCalculate = () => {
-    const calculatedWeight = weight ? parseFloat(weight) : (age ? calculateWeightFromAge(parseFloat(age)) : 0);
+	const handleCalculate = () => {
+		const calculatedWeight = weight
+			? parseFloat(weight)
+			: age
+			? calculateWeightFromAge(parseFloat(age))
+			: 0;
 
-    if (!isNaN(calculatedWeight) && calculatedWeight > 0) {
-      setShowPrescription(true);
-    } else {
-      // Handle invalid input or show an error message
-      setShowPrescription(false);
-    }
-  };
+		if (!isNaN(calculatedWeight) && calculatedWeight > 0) {
+			setShowPrescription(true);
+		} else {
+			setShowPrescription(false);
+		}
+	};
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      handleCalculate();
-    }
-  };
+	const handleKeyPress = (e) => {
+		if (e.key === "Enter") {
+			handleCalculate();
+		}
+	};
 
-  const handleAgeChange = (e) => {
-    const newAge = parseFloat(e.target.value);
+	const handleAgeChange = (e) => {
+		const newAge = parseFloat(e.target.value);
 
-    if (!isNaN(newAge)) {
-      setAge(newAge);
-      setWeight('');
-      handleCalculate();
-    } else {
-      // Handle invalid age input
-      setAge('');
-      setShowPrescription(false);
-    }
-  };
+		if (!isNaN(newAge)) {
+			setAge(newAge);
+			setWeight("");
+			handleCalculate();
+		} else {
+			setAge("");
+			setShowPrescription(false);
+		}
+	};
 
-  useEffect(() => {
-    // Recalculate prescription when either weight or age is updated
-    handleCalculate();
-  }, [weight, age]);
+	const handleDiagnosisChange = (selectedDiagnosis) => {
+		setSelectedDiagnosis(selectedDiagnosis);
+		setShowPrescription(false);
+	};
 
-  return (
-    <div className="container mx-auto text-start min-h-screen text-blue-700 bg-slate-50 bg-cover p-4">
-      <h1 className="text-4xl font-bold m-4">Dose Calculator</h1>
+	useEffect(() => {
+		handleCalculate();
+	}, [weight, age, selectedDiagnosis]);
 
-      <div className="w-full p-4">
-        <label className="block text-sm font-medium text-blue-600">Weight (kg)</label>
-        <input
-          type="number"
-          className="p-2 mt-2 border rounded-lg w-full"
-          value={weight}
-          onChange={(e) => setWeight(e.target.value)}
-          onKeyPress={handleKeyPress}
-        />
-      </div>
+	return (
+		<div className="container mx-auto text-start min-h-screen text-blue-700 bg-slate-50 bg-cover p-4">
+			<h1 className="text-4xl font-bold m-4">Dose Calculator</h1>
 
-      <div className="w-full p-4">
-        <label className="block text-sm font-medium text-blue-600">Age (years)</label>
-        <input
-          type="number"
-          className="p-2 mt-2 border rounded-lg w-full"
-          value={age}
-          onChange={handleAgeChange}
-          onKeyPress={handleKeyPress}
-        />
-      </div>
+			<div className="w-full p-4">
+				<label className="block text-sm font-medium text-blue-600">
+					Weight (kg)
+				</label>
+				<input
+					type="number"
+					className="p-2 mt-2 border rounded-lg w-full"
+					value={weight}
+					onChange={(e) => setWeight(e.target.value)}
+					onKeyPress={handleKeyPress}
+				/>
+			</div>
 
-      {showPrescription && <Prescription weight={parseFloat(weight)} age={parseFloat(age)} />}
-    </div>
-  );
+			<div className="w-full p-4">
+				<label className="block text-sm font-medium text-blue-600">
+					Age (years)
+				</label>
+				<input
+					type="number"
+					className="p-2 mt-2 border rounded-lg w-full"
+					value={age}
+					onChange={handleAgeChange}
+					onKeyPress={handleKeyPress}
+				/>
+			</div>
+
+			<div className="w-full p-4">
+				<label className="block text-sm font-medium text-blue-600">
+					Diagnosis
+				</label>
+				<select
+					className="p-2 mt-2 border text-sm rounded-lg w-full"
+					value={selectedDiagnosis}
+					onChange={(e) => handleDiagnosisChange(e.target.value)}
+				>
+					{diagnosesData.map((diagnosis) => (
+						<option key={diagnosis.value} value={diagnosis.value}>
+							{diagnosis.label}
+						</option>
+					))}
+				</select>
+			</div>
+
+			{showPrescription && (
+				<Prescription
+					diagnosis={selectedDiagnosis}
+					weight={parseFloat(weight)}
+					age={parseFloat(age)}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/components/Prescription.jsx
+++ src/components/Prescription.jsx
@@ -1,79 +1,67 @@
-// Prescription.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsCheck, BsCopy } from "react-icons/bs";
+import diagnosesData  from '../diagnoses.json';
 
-const Prescription = ({ weight, age }) => {
-	const [isCopied, setIsCopied] = useState(false);
-	const calculateWeight = () => {
-		if (weight) {
-			return weight;
-		} else if (age) {
-			// Placeholder logic for calculating weight from age
-			return 2 * age + 8;
-		}
-		return 0; // Default value if neither weight nor age is available
-	};
+const Prescription = ({ diagnosis, weight, age }) => {
+  const [isCopied, setIsCopied] = useState(false);
 
-	const y = calculateWeight();
+  const calculateWeight = () => {
+    if (weight) {
+      return weight;
+    } else if (age) {
+      return 2 * age + 8;
+    }
+    return 0;
+  };
 
-	const calculateDose = (factor, weight) => {
-		const dose = factor * weight;
-		return dose.toFixed(2);
-	  };
+  const calculateDose = (factor, weight) => {
+    const dose = factor * weight;
+    return dose.toFixed(2);
+  };
 
-	const copyToClipboard = () => {
-		const codeElement = document.getElementById('prescriptionCode');
-		if (codeElement) {
-		  const prescriptionText = codeElement.textContent;
-	  
-		  navigator.clipboard.writeText(prescriptionText).then(() => {
-			setIsCopied(true);
-			setTimeout(() => setIsCopied(false), 2000); // Reset copied state after 2 seconds
-		  });
-		}
-	  };
+  const copyToClipboard = () => {
+    const codeElement = document.getElementById('prescriptionCode');
+    if (codeElement) {
+      const prescriptionText = codeElement.textContent;
+      navigator.clipboard.writeText(prescriptionText).then(() => {
+        setIsCopied(true);
+        setTimeout(() => setIsCopied(false), 2000);
+      });
+    }
+  };
 
-	return (
-		<div className="m-4">
-			<h2 className="text-2xl font-bold mb-4">Prescription</h2>
-			<pre className="pl-2 pr-8 py-2 bg-blue-500 text-white rounded-lg relative overflow-hidden">
-				<button
-					className="flex items-center text-white m-1 px-4 py-2 absolute top-0 right-0 text-xs sm:text-sm bg-blue-500 rounded-xl"
-					onClick={copyToClipboard}
-				>
-					{isCopied ? (
-						<BsCheck className="mr-2" />
-					) : (
-						<BsCopy className="mr-2" />
-					)}
-					{isCopied ? "Copied!" : "Copy"}
-				</button>
-				<code id="prescriptionCode">
-					<ul className="list-disc pl-5 p-2 italic space-y-1 text-xs sm:text-sm overflow-x-scroll">
-						<li className="">
-							IM Artemether: {calculateDose(3.2, y)} mg (od for 3/7).
-						</li>
-						<li className="">
-							IM Paracetamol: {calculateDose(15, y)} mg stat,
-							then,
-						</li>
-						<li className="">
-							Syrup Paracetamol: {calculateDose(15, y)} mg (tds
-							for 3/7).
-						</li>
-						<li className="">
-							Syrup Cefuroxime: {calculateDose(15, y)} mg (bd
-							for 5/7), if not available give:
-						</li>
-						<li className="">
-							Syrup Amoxicillin: {calculateDose(15, y)} mg (tds
-							for 5/7).
-						</li>
-					</ul>
-				</code>
-			</pre>
-		</div>
-	);
+  const renderPrescription = () => {
+    const drugs = diagnosesData.find((item) => item.value === diagnosis)?.drugs || [];
+    return drugs.map((drug, index) => (
+      <li key={index}>
+        {`${drug.form} ${drug.name}: ${calculateDose(drug.factor, calculateWeight())} ${drug.unit} (${drug.frequency} for ${drug.duration})`}
+      </li>
+    ));
+  };
+
+  return (
+    <div className="m-4">
+      <h2 className="text-2xl font-bold mb-4">Prescription</h2>
+      <pre className="pl-2 pr-8 py-2 bg-blue-500 text-white rounded-lg relative overflow-hidden">
+        <button
+          className="flex items-center text-white m-1 px-4 py-2 absolute top-0 right-0 text-xs sm:text-sm bg-blue-500 rounded-xl"
+          onClick={copyToClipboard}
+        >
+          {isCopied ? (
+            <BsCheck className="mr-2" />
+          ) : (
+            <BsCopy className="mr-2" />
+          )}
+          {isCopied ? "Copied!" : "Copy"}
+        </button>
+        <code id="prescriptionCode">
+          <ul className="list-disc pl-5 p-2 italic space-y-1 text-xs sm:text-sm overflow-x-scroll">
+            {renderPrescription()}
+          </ul>
+        </code>
+      </pre>
+    </div>
+  );
 };
 
 export default Prescription;
