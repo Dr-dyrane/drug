3b3579be4ed81af39ee4fcba8bae4ae0cb86b675
Dr-dyrane <halodyrane@gmail.com>
2023-11-19 11:13:21 +0100

    modified vol calc

--- src/components/VolumeCalculator.jsx
+++ src/components/VolumeCalculator.jsx
@@ -1,29 +1,31 @@
-// VolumeCalculator.jsx
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const VolumeCalculator = () => {
 	const [weight, setWeight] = useState("");
+	const [age, setAge] = useState("");
 	const [dosePerKg, setDosePerKg] = useState("");
 	const [dose, setDose] = useState("");
 	const [drugStrength, setDrugStrength] = useState("");
 	const [drugVolume, setDrugVolume] = useState("");
 	const [calculatedVolume, setCalculatedVolume] = useState(null);
 
-	const calculateWeight = () => {
-		if (weight) {
-			return weight;
+	// Calculate dose based on weight/age and dose per kg
+	useEffect(() => {
+		if ((weight || age) && dosePerKg) {
+			const calculatedWeight = weight ? weight : 2 * age + 8;
+			const calculatedDose = dosePerKg * calculatedWeight;
+			setDose(calculatedDose.toString());
+		} else {
+			setDose("");
 		}
-		return 0;
-	};
+	}, [weight, age, dosePerKg]);
 
-	const calculateVolume = () => {
-		const calculatedWeight = calculateWeight();
+	// Calculate volume whenever dependent inputs change
+	useEffect(() => {
+		const calculatedWeight = weight ? weight : 2 * age + 8;
 
 		if (dose && drugStrength && drugVolume) {
-			const calculatedDose = dosePerKg
-				? dosePerKg * calculatedWeight
-				: parseFloat(dose);
+			const calculatedDose = parseFloat(dose);
 			const calculatedVolume =
 				(calculatedDose / parseFloat(drugStrength)) * parseFloat(drugVolume);
 
@@ -31,23 +33,52 @@ const VolumeCalculator = () => {
 		} else {
 			setCalculatedVolume(null);
 		}
-	};
+	}, [weight, age, dose, dosePerKg, drugStrength, drugVolume]);
 
 	return (
-		<div className="snap-mandatory snap-y" style={{ scrollSnapType: 'mandatory' }}>
-			<div className="bg-blue-100 text-blue-700 p-4 min-h-screen" style={{ scrollSnapAlign: 'start' }}>
+		<div
+			className="snap-mandatory snap-y"
+			style={{ scrollSnapType: "mandatory" }}
+		>
+			<div
+				className="bg-blue-100 text-blue-700 p-4 min-h-screen"
+				style={{ scrollSnapAlign: "start" }}
+			>
 				<h2 className="text-4xl font-bold m-4">Volume Calculator</h2>
 
-				<div className="p-4 text-sm">
-					<label className="block font-medium">
-						Weight (kg) <span className="text-red-500">*</span>
-					</label>
-					<input
-						type="number"
-						className="p-2.5 mt-2 border rounded-xl w-full text-blue-700"
-						value={weight}
-						onChange={(e) => setWeight(e.target.value)}
-					/>
+				<div className="flex flex-row">
+					<div className="p-4 text-sm">
+						<label className="block font-medium">
+							Weight (kg) <span className="text-red-500">*</span>
+						</label>
+						<input
+							type="number"
+							className="p-2.5 mt-2 border rounded-xl w-full text-blue-700"
+							value={weight}
+							onChange={(e) => {
+								setWeight(e.target.value);
+								if (e.target.value === "") {
+									setAge(""); // Reset age when weight is being changed
+								}
+							}}
+						/>
+					</div>
+					<div className="p-4 text-sm">
+						<label className="block font-medium">
+							Age (years) <span className="text-red-500">*</span>
+						</label>
+						<input
+							type="number"
+							className="p-2.5 mt-2 border rounded-xl w-full text-blue-700"
+							value={age}
+							onChange={(e) => {
+								setAge(e.target.value);
+								if (e.target.value !== "") {
+									setWeight((2 * e.target.value + 8).toString());
+								}
+							}}
+						/>
+					</div>
 				</div>
 
 				<div className="p-4 text-sm">
@@ -98,19 +129,20 @@ const VolumeCalculator = () => {
 					/>
 				</div>
 
-				<div className="m-4">
+				{/* The button is no longer needed, as volume is calculated automatically */}
+				{/* <div className="m-4">
 					<button
 						className="text-white font-bold bg-blue-700 p-3 w-full rounded-xl"
 						onClick={calculateVolume}
 					>
 						Calculate Volume
 					</button>
-				</div>
+				</div> */}
 
 				{calculatedVolume !== null && (
 					<div className="m-4 mt-6 text-center bg-slate-50 p-4 italic rounded-xl">
 						<p className="text-lg font-bold">
-							{calculatedVolume} { calculatedVolume > 1 ? "mls" : "ml"}
+							{calculatedVolume} {calculatedVolume > 1 ? "mls" : "ml"}
 						</p>
 					</div>
 				)}
