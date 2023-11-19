03ab46f18344dca07d123b7aa465f2a59fce3bea
Dr-dyrane <halodyrane@gmail.com>
2023-11-19 14:28:19 +0100

    modified:   src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaLinkedin, FaGithub, FaTwitter, FaInstagram, FaTiktok, FaFacebook } fr
 
 const Footer = () => {
 	return (
-		<footer className="bg-blue-600 text-white py-6">
+		<footer className="bg-blue-600 text-white py-6 min-h-screen text-sm">
 			<div className="max-w-4xl mx-auto p-4 md:p-8 flex flex-col items-center justify-center">
 				<div className="text-center">
 					<p className="text-xl font-semibold mb-2">About</p>
