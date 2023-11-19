23b545e54cfef8f6bf653d514b4cf86ed243d7e1
Dr-dyrane <halodyrane@gmail.com>
2023-11-19 11:39:47 +0100

    modified:   src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram, FaTiktok, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
 	return (
@@ -8,11 +8,55 @@ const Footer = () => {
 				<div className="text-center">
 					<p className="text-xl font-semibold mb-2">About</p>
 					<p className="text-gray-300">
-						Dose Calculator is a simple application that calculates
-						prescription doses based on weight or age. The application allows
-						users to input either the weight or age of a child and generates a
-						prescription summary.
+						Dose Calculator is a simple application that calculates prescription
+						doses based on weight or age.<br />The application allows users to input
+						either the weight or age of a child and generates a prescription
+						summary.<br />Additionally, the Volume Calculator feature enables users
+						to calculate the required liquid volume for a given dose, drug
+						strength, and drug volume, providing a comprehensive tool for
+						medical dosage calculations.
 					</p>
+					<div className="text-gray-300 italic p-2">
+						Courtesy - Katy Brown (Volume Calculator).
+						<span role="img" aria-label="heart" className="">
+							❤️
+						</span>
+						<div className="flex flex-row items-center justify-center p-2 text-white space-x-4">
+							<a
+								href="https://instagram.com/katy.brown__/"
+								target="_blank"
+								rel="noopener noreferrer"
+								className=""
+							>
+								<FaInstagram size={20} />
+							</a>
+							<a
+								href="https://tiktok.com/@katybrown_1/"
+								target="_blank"
+								rel="noopener noreferrer"
+								className=""
+							>
+								<FaTiktok size={20} />
+							</a>
+							<a
+								href="https://twitter.com/UkaegbuKatheri1/"
+								target="_blank"
+								rel="noopener noreferrer"
+								className=""
+							>
+								<FaTwitter size={20} />
+							</a>
+							<a
+								href="https://www.facebook.com/profile.php?id=61550207461948&mibextid=LQQJ4d"
+								target="_blank"
+								rel="noopener noreferrer"
+								className=""
+							>
+								<FaFacebook size={20} />
+							</a>
+						</div>
+					</div>
+
 					<p className="text-gray-300 mb-4">
 						This is a Portfolio Project for Dyrane Franchise.
 					</p>
@@ -38,6 +82,13 @@ const Footer = () => {
 						rel="noopener noreferrer"
 					>
 						<FaTwitter size={20} />
+					</a>
+					<a
+						href="https://instagram.com/dyrane"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FaInstagram size={20} />
 					</a>
 				</div>
 				<div className="text-center mb-4">
